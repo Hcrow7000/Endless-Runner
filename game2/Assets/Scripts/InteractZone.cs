402d844a16721c50379d4ecbbf691271bb91f976
Hcrow7000 <chanminblue@gmail.com>
2025-05-15 19:55:16 +0900

    2025-05-15 (Endless-Runner 1~3)

---
+++ game2/Assets/Scripts/InteractZone.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractZone : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Collidable collidable = other.GetComponent<Collidable>();
+
+        if (collidable != null)
+        {
+            collidable.Activate();
+
+        }
+
+    }
+}
